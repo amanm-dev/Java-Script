a9cfa5e783b1cb3b58ee64726a07e9c03860b1c2
amanm-dev <amanm.dev@gmail.com>
2026-03-22 06:58:11 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null < 0);
+
+// console.log("2" == 2);
+// console.log("3" > 5);
+console.log(0 == undefined);
+// console.log(null == []);
+// console.log(null == {});
