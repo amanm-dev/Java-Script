38045926ea87270b33cb24155bfde21eb6e4af2a
amanm-dev <amanm.dev@gmail.com>
2026-03-22 06:56:29 +0000

    type coercion

--- 01_basics/03_typecasting.js
+++ 01_basics/03_typecasting.js
@@ -1,15 +1,24 @@
 let a = 123
-console.log(typeof a, a);
+// console.log(typeof a, a);
 
 a = String(a)
-console.log(typeof a, a);
+// console.log(typeof a, a);
 
 let b=true
 b = String(b)
-console.log(typeof b, b);
+// console.log(typeof b, b);
 
 let c = null, d = undefined
 c = Number(c)
 d = Number(d)
-console.log(typeof c, c);
-console.log(typeof d, d);
+// console.log(typeof c, c);
+// console.log(typeof d, d);
+
+//-----------------------------//
+
+// console.log({}+"a");
+
+console.log("2"-2*"1");
+// console.log(2+2+"1"+2+2);
+
+
