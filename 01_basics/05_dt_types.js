d49d41b03f453ebe5108e9b2ec36e595237c6d87
amanm-dev <amanm.dev@gmail.com>
2026-03-22 11:58:27 +0000

    dt types

---
+++ 01_basics/05_dt_types.js
@@ -0,0 +1,25 @@
+// node 01_basics/05_dt_types.js
+let a = 5n
+let b = 5
+// let c = a+b   // error
+// console.log(a);
+
+
+a = 5
+b = 2
+
+a = a/b
+// console.log(a);
+let f = function(){
+    console.log("hello");
+    
+}
+let c = 12n
+let arr = [1,2]
+let obj = {
+    name: "aman"
+}
+
+console.log(typeof c);
+// console.log(typeof arr);
+// console.log(typeof obj);
