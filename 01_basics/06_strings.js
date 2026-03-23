1d1384d2894072e6448ba31f705103bb400f5879
amanm-dev <amanm.dev@gmail.com>
2026-03-23 20:32:23 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,13 @@
+// node 01_basics/06_strings.js
+let str = "hello-world"
+let str2 = "aman-mishra n ajit-mishra"
+
+console.log(str.replace("hello","aman's"));
+console.log(str2.split(" n "));
+console.log(str.split(" "));
+
+let c = "  hi  "
+console.log(c.trim());
+console.log(str.substring(1,5));
+
+console.log(str.slice(3))
