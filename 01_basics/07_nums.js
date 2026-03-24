ecc427882c49d40d3ac4245bb4f4b6d780f7f3f2
amanm-dev <amanm.dev@gmail.com>
2026-03-24 12:16:06 +0000

    numbers n math

---
+++ 01_basics/07_nums.js
@@ -0,0 +1,17 @@
+// node 01_basics/07_nums.js
+let num = 5686.57
+console.log(num.toString().length);
+console.log(num.toFixed(1));
+console.log(num.toPrecision(1));
+let x = 14565466456000000
+console.log(x.toLocaleString('en-IN'));
+
+
+// ----------------------------------------- //
+
+// Math
+
+console.log(Math.round(4.667465));
+console.log(Math.floor(Math.random()*6+1)); // 1,2,3,4,5,6
+console.log(Math.floor(Math.random()*6+1));
+
