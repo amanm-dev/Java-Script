2c9d3ff01af2b785939b4f444a312247120f4bfb
amanm-dev <amanm.dev@gmail.com>
2026-03-26 14:04:21 +0000

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+// node 02_basics/01_array.js
+const arr = [0,1,[2,[3]],4,5]
+const arr2 = new Array(10,20,30,40)
+// arr2.push(60)
+// console.log(arr);
+// arr.shift()
+// arr.shift()
+// console.log(arr);
+// arr.unshift(12)
+// console.log(arr);
+// console.log(arr2);
+// let arr3 = arr2.join(' ')
+// console.log(arr3);
+
+// console.log("arr = ",arr);
+// console.log(arr.push(arr2));
+// console.log(arr);
+// console.log(arr.flat(2));
+console.log(Array.from({leng: 3, 0:10, 1:20, 2:30}))
+
+
+
+
+
+
+
