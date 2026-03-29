7ecc6fbadec6e58d42d98c6066f910de0e244b86
amanm-dev <amanm.dev@gmail.com>
2026-03-29 13:34:45 +0000

    objects 2

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,8 +1,12 @@
 // node 02_basics/01_array.js
 const obj = {
-    name: "aman mishra",
-    // "namefull": "Aman Mishra",
-    age: 21
+    inObj1: {name: "aman mishra",
+        // "namefull": "Aman Mishra",
+        age: 21
+    },
+    inObj2: {name: "ajit mishra",
+        age: 18
+    }
 }
 // console.log(obj.namefull);
 // console.log(obj["namefull"]);
@@ -17,18 +21,18 @@ const obj = {
 // let f2 = obj.f
 // f2()
 
-const obj2 = {
-    name2: "ajit mishra",
-    age2: 18
-}
-const obj3 = {
-    name3: "kabir"
-}
+// const obj2 = {
+//     name2: "ajit mishra",
+//     age2: 18
+// }
+// const obj3 = {
+//     name3: "kabir"
+// }
 
-const obj4 = Object.assign(obj,obj2,obj3)
-console.log(obj);
-console.log(obj2);
-console.log(obj4);
+// const obj4 = Object.assign(obj,obj2,obj3)
+console.log(obj[0].name);
+// console.log(obj2);
+// console.log(obj4);
 
 
 
