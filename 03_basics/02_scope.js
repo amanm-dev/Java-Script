c099e2d1b59a50d528f93aa8976ec447f92f0d46
amanm-dev <amanm.dev@gmail.com>
2026-03-29 21:28:23 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,24 @@
+function f(){
+    var a = 51
+}
+f()
+// // console.log(a);
+
+
+function f(){
+     b = 51 // global
+}
+f()
+// console.log(b);
+{
+    const x = 45
+    console.log(x);
+    var y = 675
+    var y= 678
+}
+{
+    const x = 464
+    console.log(x);
+    y = 576
+    
+}
